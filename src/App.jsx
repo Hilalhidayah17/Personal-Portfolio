b54d02aad537249762a8829398a72d78d135142c
hilal <hilalhidayah589456@gmail.com>
2025-02-03 00:21:03 +0700

    Add Navbar component import to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
-import Navbar from "./components/navbar";
+
 import Hero from "./components/Hero";
 import About from "./components/About-me";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (

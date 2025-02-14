0b2cdcb8874f9d7af91dae1c75fb56277004e941
hilal <hilalhidayah589456@gmail.com>
2025-02-14 15:04:06 +0700

    Enhance Hero and Navbar components with new styles and animations; improve responsiveness and interactivity

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,27 +3,41 @@ import Logo from "../assets/logo-header.png";
 
 export default function Navbar() {
   return (
-    <nav className="bg-black text-white px-8 md:px-16 lg:px-24">
-      <div className="container py-2 flex justify-center md:justify-between items-center">
+    <nav className="  text-white px-8 md:px-16 lg:px-24 fixed w-full z-10  ">
+      <div className="container py-2 flex justify-center md:justify-between items-center bg-white/10 backdrop-blur-sm rounded-full px-3">
         <div className="text-2xl font-bold hidden md:inline">
           <img src={Logo} alt="Logo" className="bg-black w-12 rounded-full" />
         </div>
         <div className="space-x-6 className='">
-          <a href="#home" className="hover:text-gray-400">
+          <a
+            href="#home"
+            className="hover:text-green-400 hover:underline-offset-1 hover:underline  duration-200"
+          >
             Home
           </a>
-          <a href="#about" className="hover:text-gray-400">
+          <a
+            href="#about"
+            className="hover:text-green-400 hover:underline-offset-1 hover:underline  duration-200 "
+          >
             About Me
           </a>
-          <a href="#projects" className="hover:text-gray-400">
+          <a
+            href="#projects"
+            className="hover:text-green-400 hover:underline-offset-1 hover:underline  duration-200"
+          >
             Projects
           </a>
-          <a href="#contact" className="hover:text-gray-400">
+          <a
+            href="#contact"
+            className="hover:text-green-400 hover:underline-offset-1 hover:underline  duration-200"
+          >
             Contact Me
           </a>
         </div>
         <div className="bg-gradient-to-r from-green-500 to-blue-500 hidden md:inline   transition duration-300 hover:scale-105 px-4 py-2 rounded-full">
-          <button>Connect Me</button>
+          <button>
+            <a href="#contact">Connect Me</a>
+          </button>
         </div>
       </div>
     </nav>

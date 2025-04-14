7367021bc40f2f179965a22d1c11933ea96b31cb
hilal <hilalhidayah589456@gmail.com>
2025-04-14 14:53:31 +0700

    Refactor code structure for improved readability and maintainability

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Permalink from "../assets/Permalink.png";
+import flowfin from "../assets/flowfin.png";
+import onfeet from "../assets/onfeet.png";
+import ynews from "../assets/ynews.png";
 import BookStore from "../assets/Book-Store.png";
 import Particles from "../blocks/Backgrounds/Particles/Particles";
-import { link } from "framer-motion/client";
 
 const projects = [
   {
@@ -10,24 +12,33 @@ const projects = [
     name: "Permalink",
     image: Permalink,
     technologies: "CRUD",
-    github: "https://github.com/Hilalhidayah17",
+    link: "https://github.com/Hilalhidayah17",
   },
   {
     id: 2,
     name: "Book List",
     image: BookStore,
     technologies: "MERN Stack, CRUD",
-    github: "https://github.com/Hilalhidayah17/BookStore-Mern-stack",
+    link: "https://github.com/Hilalhidayah17/BookStore-Mern-stack",
   },
   {
     id: 3,
     name: "OnFeet",
+    image: onfeet,
     technologies: "ReactJs, Redux, Firebase",
     link: "",
   },
   {
-    id: 3,
+    id: 4,
     name: "News-App",
+    image: ynews,
+    technologies: "NextJS, Typescript",
+    link: "",
+  },
+  {
+    id: 5,
+    name: "FlowFin",
+    image: flowfin,
     technologies: "NextJS, Typescript",
     link: "",
   },
@@ -35,46 +46,55 @@ const projects = [
 
 export default function Projects() {
   return (
-    <div
-      style={{ width: "100%", height: "600px", position: "relative" }}
-      className="bg-black py-36"
-    >
-      <Particles
-        particleColors={["#ffffff", "#ffffff"]}
-        particleCount={200}
-        particleSpread={10}
-        speed={0.1}
-        particleBaseSize={100}
-        moveParticlesOnHover={true}
-        alphaParticles={false}
-        disableRotation={false}
-      />
-      <div className="container mx-auto px-8 md:px-16 lg:px-24 absolute top-0">
-        <h2 className="text-4xl font-bold text-center mb-12">My Projects</h2>
-        <div className="grid grid-cols-2  gap-8 w-full h-full">
+    <div className="relative w-full bg-black py-18 overflow-hidden">
+      {/* Background particles */}
+      <div className="absolute inset-0 z-0">
+        <Particles
+          particleColors={["#ffffff", "#ffffff"]}
+          particleCount={200}
+          particleSpread={10}
+          speed={0.1}
+          particleBaseSize={100}
+          moveParticlesOnHover={true}
+          alphaParticles={false}
+          disableRotation={false}
+        />
+      </div>
+
+      {/* Konten */}
+      <div className=" z-10 container mx-auto px-6 md:px-16 lg:px-24">
+        <h2 className="text-4xl font-bold text-center text-white mb-12">
+          My <span className="text-sky-400">Projects</span>
+        </h2>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {projects.map((project) => (
             <div
               key={project.id}
-              className="bg-transparent p-6 rounded-lg hover:shadow-lg
-            transform transition-transform duration-300 hover:scale-105 border w-fit h-fit border-zinc-800"
+              className="bg-zinc-900/40 p-6 rounded-xl hover:shadow-lg border border-zinc-800 transform transition duration-300 hover:scale-105"
             >
               <img
                 src={project.image}
                 alt={project.name}
-                className="rounded-lg mb-4
-              w-full  object-cover"
+                className="rounded-lg mb-4 w-full h-48 object-cover"
               />
-              <h3 className="text-2xl font-bold mb-2">{project.name}</h3>
+              <h3 className="text-2xl font-bold text-white mb-2">
+                {project.name}
+              </h3>
               <p className="text-gray-400 mb-4">{project.technologies}</p>
-              <a
-                href={project.github}
-                className="inline-block bg-gradient-to-r
-              from-green-400 to-blue-500 text-white px-4 py-2 rounded-full"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                GitHub
-              </a>
+
+              {project.link ? (
+                <a
+                  href={project.link}
+                  className="inline-block bg-gradient-to-r from-green-400 to-blue-500 text-white px-5 py-1 rounded-full"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Link
+                </a>
+              ) : (
+                <span className="text-sm text-gray-500">No link available</span>
+              )}
             </div>
           ))}
         </div>
